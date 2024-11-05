bc2f6dfd02bdc5b4543af3d06c4b3746d990b134
Khunrambo <95111793+samabogbog@users.noreply.github.com>
2024-11-05 11:01:02 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     const month = 12;
 
+    var cap = document.getElementById('cap');
+    var cap_text = document.getElementById('cap_text');
     var m_invest = document.getElementById('m_invest');
     var m_invest_text = document.getElementById('m_invest_text');
     var APY = document.getElementById('APY');
@@ -9,15 +11,30 @@ window.onload = function() {
     var YEAR_text = document.getElementById('year_text');
     var result = document.getElementById('result');
 
+    var _cap = parseInt(cap.value).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0});
+    cap_text.value = _cap;
     var _m_invest = parseInt(m_invest.value).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0});
-    m_invest_text.value = _m_invest
+    m_invest_text.value = _m_invest;
     var _APY = parseFloat(APY.value).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2});
     APY_text.value = _APY + "%";
-    YEAR_text.value = YEAR.value
+    YEAR_text.value = YEAR.value;
     
+    cap.addEventListener('input', function(){
+        var _cap = parseInt(cap.value).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0});
+        cap_text.value = _cap;
+        if (m_invest.value > 1000000) {
+            m_invest.value = 1000000;
+            m_invest_text.value = parseInt(m_invest.value).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0});
+        }
+        if (isNaN(_m_invest.replace(/,/g, "")) === true) {
+            m_invest_text.value = 0;
+        }
+        calc();
+    });
+
     m_invest.addEventListener('input', function(){
         var _m_invest = parseInt(m_invest.value).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0});
-        m_invest_text.value = _m_invest
+        m_invest_text.value = _m_invest;
         if (m_invest.value > 1000000) {
             m_invest.value = 1000000;
             m_invest_text.value = parseInt(m_invest.value).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0});
@@ -45,18 +62,20 @@ window.onload = function() {
     function calc() {
         var this_year = 0;
         var fix_interest = (m_invest.value * month);
-
+        
         var cap_bar = document.getElementById('cap_bar');
         var int_bar = document.getElementById('int_bar');
+        var first_year = parseInt(cap.value);
 
+        var y = 1;
         for(i=1; i<=YEAR.value; i++){
-            this_year = (this_year + fix_interest) + (this_year + fix_interest) * APY.value / 100;
+            this_year =  ((first_year * y) + fix_interest + this_year) + ((((first_year * y) + fix_interest + this_year) * APY.value) / 100);
+            y = 0;
         }
         result.value = this_year.toLocaleString(undefined, {minimumFractionDigits: 1, maximumFractionDigits: 1});
-
-        var capital = m_invest.value * month * YEAR.value;
+    
+        var capital = parseInt(cap.value) + m_invest.value * month * YEAR.value;
         var interest = (this_year - capital);
-
         var total = parseFloat(capital) + parseFloat(interest);
         var _cap = (100 / total) * parseFloat(capital);
         var _int = (100 / total) * parseFloat(interest);
